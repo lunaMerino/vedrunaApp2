ff5610f1d34625add3be3e265d2e7446ebfe8eeb
lunaMerino <marialuna.garcia-arcicollar@a.vedrunasevillasj.es>
2025-02-12 14:22:49 +0100

    perfil dos pestañas ok

--- src/screens/tabs/Profile.js
+++ src/screens/tabs/Profile.js
@@ -1,6 +1,5 @@
 import { Alert, View, Text, StyleSheet, Image, FlatList, TouchableOpacity, Dimensions, ActivityIndicator } from 'react-native'
 import React, { useState, useEffect } from 'react'
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view'
 import { theme } from '../theme'
 import { API_IP, API_PORT } from '@env';
 // npm install react-native-tab-view
@@ -12,18 +11,12 @@ export function Profile({ route }) {
   const apiURL = `http://${API_IP}:${API_PORT}`;
   
   const { user_id } = route.params || {};
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: 'posts', title: 'Publicaciones', image: require('../../../assets/grid.png') },
-    { key: 'likes', title: 'Me gusta', image: require('../../../assets/heartFull.png') },
-  ]);
-  // Alert.alert('routes', JSON.stringify(routes));
   const [posts, setPosts] = useState([]);
   const [likedPosts, setLikedPosts] = useState([]);
-  const [loadingPosts, setLoadingPosts] = useState(true);
-const [loadingLikes, setLoadingLikes] = useState(true);
+  const [showLiked, setShowLiked] = useState(false);
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
-
     const fetchPublicaciones = async () => {
       try {
         // const response = await fetch('http://192.168.1.23:8080/proyecto01/publicaciones');
@@ -32,17 +25,14 @@ const [loadingLikes, setLoadingLikes] = useState(true);
         // Filtrar publicaciones del usuario
         const userPosts = data.filter(post => post.user_id === user_id);
         setPosts(userPosts);
-        setLoadingPosts(false);
 
         // Filtrar publicaciones donde el usuario ha dado "me gusta"
         const userLikedPosts = data.filter(post => post.like.includes(user_id));
         setLikedPosts(userLikedPosts);
-        setLoadingLikes(false);
       } catch (error) {
         console.error('Error al obtener publicaciones:', error);
       } finally {
-        setLoadingPosts(false);
-        setLoadingLikes(false);
+        setLoading(false);
       }
     };
 
@@ -56,95 +46,73 @@ const [loadingLikes, setLoadingLikes] = useState(true);
     </View>
   );
 
-  const renderPosts = () => (
-    <View key="posts">
-    {loadingPosts ? (
-      <ActivityIndicator size="large" color={theme.colors.green} />
-    ) : (
-      <FlatList data={posts} keyExtractor={item => item.id} renderItem={renderItem} numColumns={3} />
-    )}
-  </View>
-  );
 
-  const renderLikes = () => (
-    <View key="likes">
-      {loadingLikes ? (
-        <ActivityIndicator size="large" color={theme.colors.green} />
-      ) : (
-        <FlatList data={likedPosts} keyExtractor={item => item.id} renderItem={renderItem} numColumns={3} />
-      )}
-    </View>
-  );
-
-  const renderScene = ({ route }) => {
-    switch (route.key) {
-      case 'posts':
-        return renderPosts();
-      case 'likes':
-        return renderLikes();
-      default:
-        return null;
+  const renderFlatList = () => {
+    if (loading) {
+      return <ActivityIndicator size="large" color={theme.colors.green} />;
     }
+    return (
+      <FlatList
+        data={showLiked ? likedPosts :posts}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        numColumns={3}
+        style={styles.flatList}
+      />
+    );
   };
-  
 
 
 
   return (
     
     <View style={styles.container}>
-    {/* Cabecera */}
-    <View style={styles.header}>
-      <View style={styles.header1}>
-        <View style={styles.avatarBase}>
-          <Image source={require('../../../assets/joseCarlos.jpg')} style={styles.avatar} />
+      {/* Cabecera */}
+      <View style={styles.header}>
+        <View style={styles.header1}>
+          <View style={styles.avatarBase}>
+            <Image source={require('../../../assets/joseCarlos.jpg')} style={styles.avatar} />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.textHeader1}>{posts.length}</Text>
+            <Text style={styles.textHeader2}>publicaciones</Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.textHeader1}>25</Text>
+            <Text style={styles.textHeader2}>seguidores</Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.textHeader1}>25</Text>
+            <Text style={styles.textHeader2}>seguidos</Text>
+          </View>
         </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.textHeader1}>{posts.length}</Text>
-          <Text style={styles.textHeader2}>publicaciones</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.textHeader1}>25</Text>
-          <Text style={styles.textHeader2}>seguidores</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.textHeader1}>25</Text>
-          <Text style={styles.textHeader2}>seguidos</Text>
+        <View style={styles.header2}>
+          <View style={styles.infoUser}>
+            <Text style={styles.username}>José Carlos</Text>
+            <Text style={styles.email}>josecarlos.moreno@a.vedrunasevillasj.es</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            {/* Botones para alternar entre Mis Publicaciones y Publicaciones que me gustan */}
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity 
+                style={[styles.button, !showLiked && styles.activeButton]} 
+                onPress={() => setShowLiked(false)}
+              >
+                <Image source={require('../../../assets/grid.png')} style={styles.iconButton} />
+              </TouchableOpacity>
+              <TouchableOpacity 
+                style={[styles.button, showLiked && styles.activeButton]} 
+                onPress={() => setShowLiked(true)}
+              >
+                <Image source={require('../../../assets/heartFull.png')} style={styles.iconButton} />
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </View>
-      <View style={styles.header2}>
-        <View style={styles.infoUser}>
-          <Text style={styles.username}>José Carlos</Text>
-          <Text style={styles.email}>josecarlos.moreno@a.vedrunasevillasj.es</Text>
-        </View>
-      </View>
+        {/* FlatList que muestra las publicaciones según el valor de `showLiked` */}
+        {renderFlatList()}
     </View>
-
-    {/* Pestañas */}
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: Dimensions.get('window').width }}
-      lazy
-      renderTabBar={props => (
-        <TabBar
-        {...props}
-        indicatorStyle={styles.indicator}
-        style={styles.tabBar}
-        renderLabel={() => null}  // Evita que el texto se muestre en las pestañas
-        renderIcon={({ route, focused  }) => (
-          // <Image
-          //   source={route.image}  // Usamos la imagen del objeto `route`
-          //   style={styles.tabImage}
-          // />
-          <Text style={[styles.tabLabel, focused ? styles.activeTab : styles.inactiveTab]}>{route.title}</Text>
-
-            )}
-          />
-      )}
-    />
-  </View>
 );
 }
 
@@ -157,7 +125,7 @@ container: {
   backgroundColor: theme.colors.blackish 
 },
 header: { 
-  height: '30%', 
+  height: '33%', 
   backgroundColor: theme.colors.blackish 
 },
 header1: { 
@@ -165,7 +133,8 @@ header1: {
   justifyContent: 'center', 
   alignItems: 'center', 
   gap: 18, 
-  marginTop: 40 },
+  marginTop: 40 
+},
 avatarBase: { width: 75, 
   height: 75, 
   backgroundColor: theme.colors.green, 
@@ -192,10 +161,11 @@ textHeader2: {
 },
 header2: { 
   marginTop: 20,
-  marginLeft:40,
+  flex: 1,
+  justifyContent: 'space-between'
 },
 infoUser: { 
-
+  marginLeft:40,
 },
 username: { 
   fontSize: 14, 
@@ -206,11 +176,34 @@ tabLabel: { fontSize: 14, textTransform: 'capitalize' },
 activeTab: { color: theme.colors.green, fontWeight: 'bold' },
 inactiveTab: { color: theme.colors.lightGray },
 
+buttonContainer: { 
+  flexDirection: 'row', 
+  justifyContent: 'center', 
+  alignItems: 'center',
+  gap: 50,
+},
+button: { 
+  paddingVertical: 8, 
+  paddingHorizontal: 16,
+  marginBottom: 4,
+  width: 70,
+  alignItems: 'center', 
+  justifyContent: 'center',
+  // borderBottomWidth: 1,
+  // borderBottomColor: theme.colors.lightGray,
+},
+activeButton: { 
+  borderBottomWidth: 1,
+  borderBottomColor: theme.colors.lightGray,
+},
+flatList: {
+  marginLeft: 3,
+},
 postContainer: { 
   flex: 1, 
   margin: 1,
   alignItems: 'center',
-  width: 105, 
+  maxWidth: 126,
   height: 115, 
 },
 postImage: { 
@@ -220,24 +213,20 @@ postImage: {
   borderWidth: 1,
   borderColor: theme.colors.lightGray,
 },
-tabLabelContainer: {
-  flexDirection: 'row',
-  alignItems: 'center',
-},
 tabImage: {
   width: 20,  // Ajusta según lo que necesites
   height: 20, // Ajusta según lo que necesites
   marginRight: 5, // Espacio entre la imagen y el texto si es necesario
 },
-tabImage: {
+iconButton: {
   width: 24,
   height: 24,
   resizeMode: 'contain',
 },
-tabBar: {
-  backgroundColor: theme.colors.blackish,
-  height: 50,
-},
+// tabBar: {
+//   backgroundColor: theme.colors.blackish,
+//   height: 50,
+// },
 indicator: {
   backgroundColor: theme.colors.lightGray,
   height: 1,
